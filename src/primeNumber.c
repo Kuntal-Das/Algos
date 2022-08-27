f74333228d62175f2db57236d5a5f5948c866a1c
Kuntal Das <25ckuntaldas@gmail.com>
2022-08-28 00:03:28 +0530

    optimized prime!?

--- src/primeNumber.c
+++ src/primeNumber.c
@@ -2,7 +2,7 @@
 
 void main()
 {
-    unsigned long long int n = 0, i = 0, j = 0, devideCount = 0, pCount = 0, temp = 0;
+    unsigned long long int n = 0, i = 0, j = 0, devideCount = 0, pCount = 0;
     unsigned long long int prime = 2;
     printf("Enter interger n: ");
     scanf("%lld", &n);
@@ -12,20 +12,18 @@ void main()
         devideCount = 0;
         for (j = 2; j <= i / 2; j++)
         {
-            if (i % j == 0)
+            if (i % j == 0){
                 devideCount++;
+                break;
+            }
         }
 
         if (devideCount == 0)
         {
             pCount++;
 
-            if (pCount == n - 1 || pCount == n || pCount == n + 1)
+            if (pCount == n || pCount == n + 1 || pCount == n + 2)
                 printf("%lldth Prime No : %lld\n", pCount - 1, i);
         }
-        // else
-        //     printf("%lld is not Prime Number\n\n", i);
     }
-
-    // printf("\n%lldth Prime No is: %lld\n\n", n, prime);
 }
